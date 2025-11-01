3c90160406822321a8a557be100b91fec342be61
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2025-11-01 16:18:35 -0300

    Add explanatory comment

--- symline.sh
+++ symline.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+# Run this script to get a full listing of where each symbol is defined.
+
 find -name '*.s' -exec grep -nP '^[A-Za-z0-9_]+:' {} + |
 	(while IFS=: read -r file line func rest; do
 		printf '%-40s %5u %s\n' "$func" "$line" "${file#./}"
